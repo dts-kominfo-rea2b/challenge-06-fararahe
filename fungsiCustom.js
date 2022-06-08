c3bdd166525d0ebbccbbb0e4cb9bf30f67343f45
fararahe <faradyna.rahma@gmail.com>
2022-06-08 15:29:30 +0700

    Tantangan: Callback Part 2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,47 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let files = [file1, file2, file3];
+  let results = [];
+
+  files.forEach(item => {
+    fs.readFile(
+      item,
+      {
+        encoding : 'utf-8',
+      },
+      (err, data) => {
+        if(err){
+          return fnCallback(err, null);
+        }
+
+        let jsonData = JSON.parse(data);
+
+        if(jsonData.message != undefined){
+          results.push(secondWords(jsonData.message));
+        }else if(Array.isArray(jsonData)){
+          jsonData.forEach(item => {
+            if(item.message != undefined){
+              results.push(secondWords(item.message));
+            }else{
+              results.push(secondWords(item.data.message));
+            }
+          });
+        }
+
+        if(results.length == 3){
+          return fnCallback(null, results);
+        }
+      }
+    );
+  });
+};
+
+const secondWords = (data) => {
+  let words = data.split(' ');
+  return words[1];
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
